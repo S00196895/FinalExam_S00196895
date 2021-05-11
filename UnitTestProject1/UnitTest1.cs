78611fa60bf29d9537fb2b493636cb0bb76791f1
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-05-11 09:49:20 +0100

    Created a unit test on decrease price

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BartlomiejSajdok_S00196895;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDecreasePrice()
+        {
+            //arrange
+            Game g1 = new Game("Nier", 9.5, 59.99m);
+            decimal expectedPrice = 49.99m;
+            //act
+            g1.DecreasePrice(10.00m);
+
+            Assert.AreEqual(expectedPrice, g1.Price);
+        }
+    }
+}
