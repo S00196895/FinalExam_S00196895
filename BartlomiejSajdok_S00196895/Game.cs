55258ad0463a70e4b9af268da2e7b98294c44390
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-05-11 09:38:42 +0100

    Created Game properties

---
+++ BartlomiejSajdok_S00196895/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BartlomiejSajdok_S00196895
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public double MetricScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string GameImage { get; set; }
+    }
+}
